103e7e355275202c56ad3d33011ae5d7252e5761
Erich Kopp <40647127+erichkopp@users.noreply.github.com>
2019-09-03 23:19:24 -0400

    Add files via upload
    Add more navigation

--- erichkoppfilm/scripts.js
+++ erichkoppfilm/scripts.js
@@ -3,17 +3,22 @@ $(document).ready(function() {
 	$('.photo').click(function() {
 
 		$('#modal').attr('style', 'display: block');
-		$('#modal-img').attr('src', $(this).find('img').attr('src'))
+		$('#modal-img').attr('src', $(this).find('img').attr('src'));
 
 
 		$('#close').click(function() {
-			$('#modal').attr('style', 'display: none')
+			$('#modal').attr('style', 'display: none');
 		});
 
 		$('#modal').click(function() {
-			$('#modal').attr('style', 'display: none')
+			$('#modal').attr('style', 'display: none');
 		});
 
+	});
+
+
+	$('#logo').click(function() {
+		window.location.href = 'index.html';
 	});
 
 });
