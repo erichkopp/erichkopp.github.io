e989608d176078e6c2e92c8f4bd558957e9b20c5
Erich Kopp <40647127+erichkopp@users.noreply.github.com>
2019-09-09 21:12:06 -0400

    Add files via upload
    Change to main page

---
+++ scripts.js
@@ -0,0 +1,169 @@
+$(document).ready(function() {
+
+
+	// KILL DESKTOP ANIMATIONS FOR MOBILE
+	if ($(window).width() <= 750) {
+		$('#scrolling-wrapper').removeClass('animated slideInRight delay-1s');
+	};
+
+
+	// CLICK TO RELOAD PAGE
+	$('#logo').click(function() {
+		if ($(window).width() > 750){
+    		location.reload();
+    	};
+	});
+
+
+	// OPEN MODAL IMAGE
+	$('.photo').click(function() {
+		if ($(window).width() > 750) {
+			$('#modal').attr('style', 'display: block');
+			$('#modal-img').attr('src', $(this).find('img').attr('src'));
+			$('#close').click(function() {
+				$('#modal').attr('style', 'display: none');
+			});
+			$('#modal').click(function() {
+				$('#modal').attr('style', 'display: none');
+			});
+		};
+	});
+
+	
+	// MORE-ARROW DISAPPEARS AFTER SCROLLING (DESKTOP)
+	$("#scrolling-wrapper").on('scroll', function() {
+		if ($(window).width() > 750) {
+			let x = $("#first-img").position();
+			if (x.left <= -500) {
+				$('#more-arrow').fadeOut(500);
+			};	
+		};
+	});
+
+
+	// BACK-TO-TOP BUTTON APPEARS AND DISAPPEARS DEPENDING
+	// ON SCROLL POSITION (MOBILE)
+	$("#scrolling-wrapper").on('scroll', function() {
+		if ($(window).width() <= 750) {
+			let x = $("#first-img").position();
+			if (x.top <= -500) {
+				$('#scroll-top-mobile').attr('style', 'display: block');
+				$('#scroll-top-mobile').removeClass('fadeOut');
+				$('#scroll-top-mobile').addClass('fadeIn');
+				$('#scroll-top-mobile').fadeIn(500);
+			} else if (x.top < 100) {
+				// $('#scroll-top-mobile').addClass('fadeOut');
+				$('#scroll-top-mobile').attr('style', 'display: none');
+			};
+		};
+	});
+
+
+	// BACK-TO-TOP BUTTON (MOBILE)
+	$('#scroll-top-mobile').click(function() {
+		$("#scrolling-wrapper").animate({ scrollTop: 0 }, "slow");
+	});
+
+
+	// HIDE/ADD RELEVANT ELEMENTS WHEN SWITCHING BETWEEN DESKTOP & MOBILE
+	$(window).resize(function() {
+		if ($(window).width() > 750) {
+			$('#scrolling-wrapper').addClass('animated slideInRight delay-1s');
+			$('#scroll-top-mobile').attr('style', 'display: none');
+			$('#mobile-menu').attr('style', 'display: none');
+			$('#hamburger').attr('style', 'display: none');
+			$('#close-mobile').attr('style', 'display: none');
+		} else if ($(window).width() <= 750) {
+			$('#scrolling-wrapper').removeClass('animated slideInRight delay-1s');
+			$('#scroll-top-mobile').attr('style', 'display: none');
+			$('#mobile-menu').attr('style', 'display: none');
+			$('#hamburger').attr('style', 'display: initial');
+			$('#close-mobile').attr('style', 'display: none');			
+		};
+	});
+
+
+	// SHOW CONTACT PAGE / HIDE WORK PAGE (DESKTOP)
+	$('#contact').click(function() {
+		$('#scrolling-wrapper').removeClass('delay-1s slideInRight');		
+		$('#scrolling-wrapper').addClass('slideOutRight faster');
+		setTimeout(function() {
+			$('#scrolling-wrapper').hide();
+			$('#contact-page').attr('style', 'display: block');
+			$('#contact-page').addClass('animated slideInRight');
+			setTimeout(function() {
+				$('#contact-header').attr('style', 'color: #000000');
+				$('#contact-header').text('HIT ME UP!');
+			}, 800);
+		}, 800);
+	});
+
+
+	// SHOW WORK PAGE / HIDE CONTACT PAGE (DESKTOP)
+	$('#work').click(function() {
+		$('#contact-page').removeClass('slideInRight');
+		$('#contact-page').addClass('slideOutRight faster');
+		setTimeout(function() {
+			$('#contact-page').hide();
+			$('#scrolling-wrapper').removeClass('slideOutRight faster');
+			$('#scrolling-wrapper').attr('style', 'display: block');
+			$('#scrolling-wrapper').addClass('slideInRight');
+			$('#contact-page').removeClass('slideOutRight');
+			$('#contact-header').attr('style', 'color: transparent');
+		}, 500);
+	});
+
+
+	// SHOW WORK PAGE / HIDE CONTACT PAGE (MOBILE)
+	$('#work-mobile').click(function() {
+		$('#mobile-menu').removeClass('slideInRight')
+		$('#mobile-menu').addClass('slideOutRight');
+		setTimeout(function() {
+			$('#close-mobile').attr('style','display: none');
+			$('#hamburger').attr('style','display: block');
+			$('#mobile-menu').removeClass('slideOutRight');
+			$('#mobile-menu').attr('style', 'display: none');
+		},500);
+		$('#contact-page').attr('style', 'display: none');
+		$('#contact-header').attr('style', 'color: transparent');
+		$('#scrolling-wrapper').attr('style', 'display: block');
+	});
+
+
+	// SHOW CONTACT PAGE / HIDE WORK PAGE (MOBILE)
+	$('#contact-mobile').click(function() {
+		$('#mobile-menu').removeClass('slideInRight')
+		$('#mobile-menu').addClass('slideOutRight');
+		setTimeout(function() {
+			$('#close-mobile').attr('style','display: none');
+			$('#hamburger').attr('style','display: block');
+			$('#mobile-menu').removeClass('slideOutRight');
+			$('#mobile-menu').attr('style', 'display: none');
+		},500);
+		$('#scrolling-wrapper').attr('style', 'display: none');
+		$('#scroll-top-mobile').attr('style', 'display: none');
+		$('#contact-page').attr('style', 'display: block');
+		$('#contact-header').attr('style', 'color: #000000');
+		$('#contact-header').text('HIT ME UP!');
+	});
+
+
+	// OPEN/CLOSE SLIDE-OUT HAMBURGER MENU (MOBILE)
+	$('#hamburger').click(function() {
+		$('#mobile-menu').attr('style', 'display: block');
+		$('#mobile-menu').addClass('slideInRight');
+		$('#hamburger').attr('style', 'display: none');
+		// $('#close-mobile').attr('style', 'z-index: 5');
+		$('#close-mobile').attr('style', 'display: block');
+		$('#close-mobile').click(function() {
+			$('#mobile-menu').removeClass('slideInRight')
+			$('#mobile-menu').addClass('slideOutRight');
+			setTimeout(function() {
+				$('#close-mobile').attr('style','display: none');
+				$('#hamburger').attr('style','display: block');
+				$('#mobile-menu').removeClass('slideOutRight');
+				$('#mobile-menu').attr('style', 'display: none');
+			}, 500);
+		});
+	});
+});
